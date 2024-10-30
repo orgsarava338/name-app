cf49f56099277b71d9dab28b09c044a68efcf5bb
sarava338 <sarava338@gmail.com>
2024-10-30 23:50:03 +0530

    verifyToken fn changed

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export async function verifyToken(req, res, next) {
     let token;
-    const authHeader = req.headers.Autherization || req.headers.Autherization;
+    const authHeader = req.headers.Auth0rization || req.headers.authorization;
     
     if(authHeader && authHeader.startsWith('Bearer')) 
         token = authHeader.split(' ')[1];
@@ -10,18 +10,15 @@ export async function verifyToken(req, res, next) {
     if(!token) return res.status(401).json({message: 'No Token. Autherization denied.'});
     
     try {
-        const decode = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decode;
-        console.log('decoded user : ', user);
+        req.user = jwt.verify(token, process.env.JWT_SECRET);
         next();
-
     } catch (error) {
         console.error(error);
         res.status(400).json({message: 'token not valid'});
     }
 }
 
-export async function authorizeRoles(...roles) {
+export function authorizeRoles(...roles) {
     return (req, res, next) => {
         if(!roles.includes(req.user.role)) return res.status(403).json({message: 'Access Denied.'});
         next();
