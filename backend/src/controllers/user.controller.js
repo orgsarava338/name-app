d73745f59c64f85c3725dc6c80fdf3b01717fed3
sarava338 <sarava338@gmail.com>
2024-10-30 23:55:48 +0530

    modified error message for default function

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,19 +1,19 @@
 export async function createUser(req, res) {
-    throw new Error(`implement ${this.name}`);
+    throw new Error('implement method');
 }
 
 export async function getAllUsers(req, res) {
-    throw new Error(`implement ${this.name}`);
+    throw new Error('implement method');
 }
 
 export async function getUser(req, res) {
-    throw new Error(`implement ${this.name}`);
+    throw new Error('implement method');
 }
 
 export async function updateUser(req, res) {
-    throw new Error(`implement ${this.name}`);
+    throw new Error('implement method');
 }
 
 export async function deleteUser(req, res) {
-    throw new Error(`implement ${this.name}`);
+    throw new Error('implement method');
 }
