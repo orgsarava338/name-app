db07c62e2c8df823b34df5fe456db8cc52f49a4e
sarava338 <sarava338@gmail.com>
2024-10-31 06:33:11 +0530

    added a middleware to validate mongoose id

--- backend/src/routes/name.route.js
+++ backend/src/routes/name.route.js
@@ -2,16 +2,17 @@ import express from "express";
 
 import { authorizeRoles, verifyToken } from "../middlewares/auth.middleware.js";
 import { createName, deleteName, getAllNames, getName, updateName } from "../controllers/name.controller.js";
+import { isValidMongooseId } from "../middlewares/mongo.middleware.js";
 
 const nameRouter = express.Router();
 
 /** PUBLIC */
 nameRouter.get('/', getAllNames);
-nameRouter.get('/:id', getName);
+nameRouter.get('/:id', isValidMongooseId, getName);
 
 /** ADMIN */
 nameRouter.post('/', verifyToken, authorizeRoles('ADMIN'), createName);
-nameRouter.put('/:id', verifyToken, authorizeRoles('ADMIN'), updateName);
-nameRouter.delete('/:id', verifyToken, authorizeRoles('ADMIN'), deleteName);
+nameRouter.put('/:id', isValidMongooseId, verifyToken, authorizeRoles('ADMIN'), updateName);
+nameRouter.delete('/:id', isValidMongooseId, verifyToken, authorizeRoles('ADMIN'), deleteName);
 
 export default nameRouter;
