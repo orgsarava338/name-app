96c4ab85ac7ede4ec95fff341fd10af2c214be42
sarava338 <sarava338@gmail.com>
2024-11-10 09:53:34 +0530

    ci-1 check

--- frontend/src/components/Letters.tsx
+++ frontend/src/components/Letters.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 import Header from "./Header"
 
 import { NameContext } from "../context/NameContext"
-import { Container, Stack } from "react-bootstrap"
+import { Button, Container, Stack } from "react-bootstrap"
 
 const uyirgal = ['அ', 'ஆ', 'இ', 'ஈ', 'உ', 'ஊ', 'எ', 'ஏ', 'ஐ', 'ஒ', 'ஓ', 'ஔ']
 const meigal = ['க', 'ங', 'ச', 'ஞ', 'ட', 'ண', 'த', 'ந', 'ப',
@@ -15,30 +15,30 @@ export default function Letters() {
     const { setSearch } = useContext(NameContext)
 
     return (
-    <Container as='section'>
+    <Container as='section' className="my-4">
         <Header>
             <h2>எழுத்துகள்</h2>
         </Header>
 
         <Container>
             {/* <h3>உயிரெழுத்துகள்</h3> */}
-            <Stack direction="horizontal">
+            <Stack direction="horizontal" gap={3} className="mb-3">
                 {uyirgal.map(uyir => 
                     <Link to={`/name/${uyir}`} key={uyir} state={uyir} 
                         onClick={() => {setSearch(uyir)}}
                     >
-                        <span className="letter">{uyir}</span>
+                        <Button variant="primary">{uyir}</Button>
                     </Link>
                 )}
             </Stack>
 
             {/* <h3>மெய்யெழுத்துகள்</h3> */}
-            <Stack direction="horizontal">
+            <Stack direction="horizontal" gap={3}>
                 {meigal.map(mei => 
                     <Link to={`/name/${mei}`} key={mei} state={mei}
                         onClick={() => {setSearch(mei)}}
                     >
-                        <span className="letter">{mei}</span>
+                        <Button variant="secondary">{mei}</Button>
                     </Link>
                 )}
             </Stack>
